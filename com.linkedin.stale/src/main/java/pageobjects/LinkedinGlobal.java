203ade51e3c4f971ec47cdeb403606f23471a0f5
Sandeep Kumar <sandeepk.at2@gmail.com>
2020-10-13 17:13:56 -0400

    Update LinkedinGlobal.java
    Privacy variable initialization updated

--- com.linkedin.stale/src/main/java/pageobjects/LinkedinGlobal.java
+++ com.linkedin.stale/src/main/java/pageobjects/LinkedinGlobal.java
@@ -9,7 +9,7 @@ public class LinkedinGlobal {
 
 		public static final String LINKEDIN_LOGIN_URL = "https://www.linkedin.com/login";
 		public static final String USER_NAME = "sandeep@gmail.com"; //please change to valid username when executing
-		public static final String PASSWORD = "Bsk14147"; // please change to valid password when executing
+		public static final String PASSWORD = "something"; // please change to valid password when executing
 		public static final String INVALID_USER_NAME = "demo@s.com";
 		public static final String INVALID_PASSWORD = "dopemam";
 	
